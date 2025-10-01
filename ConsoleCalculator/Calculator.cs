8fc4ccd0780ffdc90daaf5b031b61293e958844d
Eugeny Vlasov <eugeny.vlasov@gmail.com>
2025-10-01 04:01:51 +0300

    Shunting Yard algorithm implemented. RPN algorythm added

--- ConsoleCalculator/Calculator.cs
+++ ConsoleCalculator/Calculator.cs
@@ -6,6 +6,7 @@ using System.Data;
 using System.Globalization;
 using System.Linq;
 using System.Net;
+using System.Reflection;
 using System.Runtime.ExceptionServices;
 using System.Text;
 using System.Threading.Tasks;
@@ -15,9 +16,17 @@ namespace ConsoleCalculator
     /// <summary>
     /// Provides basic math operations for the calculator.
     /// </summary>
+    /// 
     public class Calculator
     {
         private readonly List<string> tokens;
+        private readonly Dictionary<string, int> priorities = new Dictionary<string, int>
+        {
+            ["+"] = 1,
+            ["-"] = 1,
+            ["*"] = 2,
+            ["/"] = 2,
+        };
 
         public Calculator(List<string> tokens)
         {
@@ -31,71 +40,113 @@ namespace ConsoleCalculator
         public double Evaluate()
         {
             double result = 0;
-            ProcessMulDiv();
-            result = ProcessAddSub();
+            //ProcessMulDiv();
+            //result = ProcessAddSub();
+            List<string> tokensRPN = ShuntingYard();
+            result = EvaluateRPN(tokensRPN);
+
             return result;
         }
 
-        private void ProcessMulDiv() //Handle * and /
+
+        private List<string> ShuntingYard()
         {
-            int index = 0;
+            List<string> tokensRPN = new List<string>();
+            Stack<string> stack = new Stack<string>();
+            Stack<bool> operandsInsideParentheses = new Stack<bool>();
 
-            while (index < tokens.Count) {
-                string token = tokens[index];
-                double result = 0;
+            foreach (string token in tokens) {
 
-                if (token == "*" || token == "/") {
-                    if (index <= 0 || index >= tokens.Count - 1) // no operands for operation
-                        throw new InvalidTokenException("No operands for operation");
-
-                    double left = ParseOperand(tokens[index - 1]);
-                    double right = ParseOperand(tokens[index + 1]);
-
-                    if (token == "*")
-                        result = left * right;
-
-                    if (token == "/") {
-                        if (right == 0.0)
-                            throw new DivideByZeroCalculatorException("Division by zero");
-                        result = left / right;
+                if (double.TryParse(token, NumberStyles.Any, CultureInfo.InvariantCulture, out double value)) {
+                    tokensRPN.Add(value.ToString(CultureInfo.InvariantCulture));
+                    
+                    var tempStack = new Stack<bool>();
+                    while(operandsInsideParentheses.Count > 0) {
+                        tempStack.Push(true);
+                        operandsInsideParentheses.Pop();
                     }
-
-                    // Replace [left, operator, right] with [result]
-                    tokens[index - 1] = result.ToString();
-                    tokens.RemoveAt(index);
-                    tokens.RemoveAt(index);
-
-                    index--;
+                    while (tempStack.Count > 0)
+                        operandsInsideParentheses.Push(tempStack.Pop());
                 }
                 else
-                    index++;
+                if (token == "(") {
+                    stack.Push(token);
+                    operandsInsideParentheses.Push(false); // a new block of "(", no numbers inside
+                }
+                else
+                if (token == ")") {
+                    if (operandsInsideParentheses.Count == 0 || operandsInsideParentheses.Pop() == false)
+                        throw new InvalidTokenException("Empty parentheses");
+
+                    while (stack.Count > 0 && stack.Peek() != "(")
+                        tokensRPN.Add(stack.Pop());
+
+                    if (stack.Count == 0 || stack.Pop() != "(")
+                        throw new InvalidTokenException("Mismatched parentheses");
+                }
+                else
+                if (token == "+" || token == "-" || token == "*" || token == "/") {
+                    while (stack.Count > 0 && stack.Peek() != "(" && priorities[stack.Peek()] >= priorities[token]) {
+                        tokensRPN.Add(stack.Pop());
+                    }
+                    stack.Push(token);
+                }
+                else {
+                    throw new InvalidTokenException($"Unknown token: {token}");
+                }
             }
+
+            while (stack.Count > 0) { //Let's check that every open parenthes has closed parenthes
+                var op = stack.Pop();
+                if (op == "(")
+                    throw new InvalidTokenException("Mismatched parentheses. Too many '('");
+                tokensRPN.Add(op);
+            }
+
+            return tokensRPN;
         }
 
-        private double ProcessAddSub() //Handle + and -
+        private double EvaluateRPN(List<string> tokensRPN)
         {
-            double result = ParseOperand(tokens[0]);
-            int index = 1;
-            while(index < tokens.Count) {
-                string oper = tokens[index];
-                double right = ParseOperand(tokens[index + 1]);
+            Stack<double> stack = new Stack<double>();
 
-                if (oper == "+")
-                    result += right;
-                if (oper == "-")
-                    result -= right;
-                index += 2; //go to next operator
+            for (int i = 0; i < tokensRPN.Count; i++) {
+                if (double.TryParse(tokensRPN[i], NumberStyles.Any, CultureInfo.InvariantCulture, out double value)) {
+                    stack.Push(value);
+                }
+                else {
+                    if (stack.Count > 1) {
+                        double right = stack.Pop();
+                        double left = stack.Pop();
+                        stack.Push( Calc(left, tokensRPN[i], right) );
+                    }
+                    else {
+                        throw new InvalidTokenException("Wrong expression. Not Enough operands");
+                    }
+                }
             }
 
+            if (stack.Count != 1)
+                throw new InvalidTokenException("Wrong expression. Extra operands or operators");
+
+            return stack.Pop();
+        }
+
+        private double Calc(double left, string op, double right)
+        {
+            double result = 0;
+            switch (op) {
+                case "+": result = left + right; break;
+                case "-": result = left - right; break;
+                case "*": result = left * right; break;
+                case "/":
+                    if (right == 0)
+                        throw new DivideByZeroCalculatorException("Division by zero");
+                    result = left / right; 
+                    break;
+                default: throw new InvalidTokenException("Invalid operation");
+            }
             return result;
         }
-
-        private double ParseOperand(string token)
-        {
-            if (!double.TryParse(token, NumberStyles.Any, CultureInfo.InvariantCulture, out double value))
-                throw new InvalidNumberException(token);
-            return value;
-        }
-
     }
 }
